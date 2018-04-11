27774fe2221122fa2b44418d034bbcecbf4bb862
pascalvdkwast <pascalvdkwast@gmail.com>
2018-04-11 22:42:27 +0200

    Test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Dominion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gameboard gameboard = new Gameboard();
+
+            Console.WriteLine(gameboard.Name);
+            Console.ReadKey();
+        }
+    }
+
+    public class Gameboard
+    {
+        // Properties
+        public string Name { get; set; }
+
+        // Method
+        public string Stack()
+        {
+            return "Hi";
+        }
+
+
+        // Instance constructor
+        public Gameboard()
+        {
+            Name = "Gameboard";
+        }
+    }
+}
---
+++ Dominion/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominion
+{
+    class Dominion
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
